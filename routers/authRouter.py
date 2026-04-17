ccc4778bb3e2d41fbe6c910c109cd1892fea3bed
SayantikaDey2004 <sayantika.dey2004@gmail.com>
2026-04-18 01:19:41 +0530

    Refactor: update import paths for user, NGO signup, and login schemas in auth router

--- routers/authRouter.py
+++ routers/authRouter.py
@@ -1,11 +1,11 @@
 from fastapi import APIRouter
-from app.Schemas.Users.signUpSchema import UserSignUpSchema
-from app.Schemas.NGO.signUpSchema import NgoSignUpSchema
-from app.Schemas.logInSchema import loginSchema
-from app.Schemas.token import Token
-from app.controllers.auth.Users.userSignUp import signup_user
-from app.controllers.auth.NGO.NgoSignUp import signup_ngo
-from app.controllers.auth.LogIn import login_user
+from app.models.Users.signUpSchema import UserSignUpSchema
+from app.models.NGO.signUpSchema import NgoSignUpSchema
+from app.models.logInSchema import loginSchema
+from app.models.token import Token
+from app.services.auth.Users.userSignUp import signup_user
+from app.services.auth.NGO.NgoSignUp import signup_ngo
+from app.services.auth.LogIn import login_user
 
 router = APIRouter(prefix="/auth", tags=["Auth"])
 
